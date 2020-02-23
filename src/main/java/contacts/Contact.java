374314e648f38ead526b6b3f254c1fe3cde6ec1f
xx <buptxuxin@163.com>
2020-02-23 21:07:55 +0800

    Initial commit

---
+++ src/main/java/contacts/Contact.java
@@ -0,0 +1,49 @@
+package contacts;
+
+public class Contact {
+    private  Long id;
+    private  String firstName;
+    private  String lastName;
+    private  String phoneNumber;
+    private  String emailAddress;
+
+    public String getFirstname() {
+        return firstName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
